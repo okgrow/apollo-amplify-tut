26a9e1f5b5b0cb0fa9b42a948052e00b2cf4e115
Robert Dickert <robert.dickert@gmail.com>
2019-03-15 14:24:45 -0600

    tutorial result

---
+++ README.md
@@ -0,0 +1,14 @@
+# Apollo in Amplify Tutorial
+
+This repo contains the result of following the instructions in the post [How to set up Apollo in Amplify](https://www.okgrow.com/posts/config-apollo-in-amplify). It is not intended to be a template for projects, but if you use this code, delete `team-provider-info.json` from .gitignore.
+
+## Run
+
+You'll need an AWS profile (run `amplify configure` if not).
+
+Clone this repo, then from inside the project:
+
+```
+amplify init
+amplify push
+```
---
+++ amplify/backend/function/apollosvr/src/index.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: (parent, args, context) => {
+      return 'Hello world!';
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: true,
+    credentials: true,
+    allowedHeaders: ['authorization', 'content-type', 'x-amz-security-token', 'x-amz-date'],
+  },
+});
